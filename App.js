e2dfaa8ff503bfcdff23231527a9bd8d03ea4073
nrizh31 <naufalrizhaubi@student.uir.ac.id>
2024-11-25 22:08:09 +0700

    commit by Ryz

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Tugas Praktikum Pert.2</Text>
+      <Text style={styles.text}>Naufal Rizh</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  box1: {
+    backgroundColor: '#2E8B57', 
+    width: 100,
+    height: 100,
+  },
+  box2: {
+    backgroundColor: '#800000',
+    width: 100,
+    height: 100,
   },
 });
